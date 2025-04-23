ca31d87a011a590bdb3c6184eabbb3491f45cce9
Delacoudre willelm <willelm.delacoudre@etu.unicaen.fr>
2025-04-23 14:06:39 +0200

    premier lancement appli couleur

---
+++ tp3/src/main/java/iut/gon/tp3/GrilleController.java
@@ -0,0 +1,16 @@
+package iut.gon.tp3;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.GridPane;
+
+public class GrilleController implements Initializable{
+	private @FXML GridPane grille;
+	
+	public void initialize(URL url, ResourceBundle resource){
+		grille.setStyle("-fx-background-color: seashell");
+	}
+}
