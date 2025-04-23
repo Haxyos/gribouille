ccbbe829a4bee1fa061246dc7d86e8fd07337ad6
Delacoudre willelm <willelm.delacoudre@etu.unicaen.fr>
2025-04-23 16:39:15 +0200

    implémentation class Dialogue

--- gribouille_tp1/src/main/java/iut/gon/gribouille_tp1/Dialogues.java
+++ gribouille_tp1/src/main/java/iut/gon/gribouille_tp1/Dialogues.java
@@ -1,10 +1,21 @@
 package iut.gon.gribouille_tp1;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
+
 public class Dialogues {
-	static boolean confirmation() {
-		if () {
-			
-		}
-		return false
+	static boolean confirmation(Stage stage) {
+			      Alert sorti = new Alert(AlertType.CONFIRMATION);
+			      sorti.setContentText("Are you sure ?");
+			      sorti.getButtonTypes().add(ButtonType.YES);
+			      sorti.getButtonTypes().add(ButtonType.NO);
+			      sorti.getButtonTypes().remove(0);
+			      sorti.getButtonTypes().remove(0);
+			      if (sorti.showAndWait().orElse(ButtonType.YES) == ButtonType.YES) {
+			    	  return true;
+			      }
+		return false;
 	}
 }
