a195a20842f6cc79ff0cc7969f54429b05677446
Delacoudre willelm <willelm.delacoudre@etu.unicaen.fr>
2025-04-30 10:57:29 +0200

    ajout de la fonction dessin

--- gribouille_tp1/src/main/java/iut/gon/gribouille_tp1/App.java
+++ gribouille_tp1/src/main/java/iut/gon/gribouille_tp1/App.java
@@ -4,8 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,10 +18,12 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
-
+    private double prevX;
+    private double prevY;
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("CadreGribouille"), 640, 480);
+        Canvas dessin = (Canvas) scene.lookup("Canvas");
         stage.setScene(scene);
         stage.show();
         stage.setOnCloseRequest(event-> {
@@ -28,6 +32,16 @@ public class App extends Application {
         		event.consume();
         	}
         });
+        dessin.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+        	this.prevX = event.getX();
+        	this.prevY = event.getY();
+        });
+        dessin.addEventHandler(MouseEvent.MOUSE_DRAGGED, event -> {
+        	dessin.getGraphicsContext2D().strokeLine(prevX, prevY, event.getX(), event.getY());
+        	this.prevX = event.getX();
+        	this.prevY = event.getY();
+        });
+        
     }
 
     static void setRoot(String fxml) throws IOException {
