29bda0d510eeb1e537043b667c7c012eb8d65504
Delacoudre willelm <willelm.delacoudre@etu.unicaen.fr>
2025-06-03 16:37:05 +0200

    commencement outil

--- gribouille_tp1/src/main/java/iut/gon/gribouille_tp1/controleurs/Outil.java
+++ gribouille_tp1/src/main/java/iut/gon/gribouille_tp1/controleurs/Outil.java
@@ -1,14 +1,16 @@
 package iut.gon.gribouille_tp1.controleurs;
 
+import iut.gon.gribouille_tp1.modele.Figure;
 import javafx.scene.input.MouseEvent;
 
 public abstract class Outil {
 	protected Controleur controller;
+	protected Figure figureCourante;
 	
 	public Outil(Controleur controller) {
 		this.controller = controller;
 	}
 	
-	public abstract void onMousePress(MouseEvent evt);
-	public abstract void onMouseDrag(MouseEvent evt);
+	public abstract void onMousePress(double x, double y);
+	public abstract void onMouseDrag(double x, double y);
 }
--- gribouille_tp1/src/main/java/iut/gon/gribouille_tp1/controleurs/OutilCrayon.java
+++ gribouille_tp1/src/main/java/iut/gon/gribouille_tp1/controleurs/OutilCrayon.java
@@ -1,5 +1,6 @@
 package iut.gon.gribouille_tp1.controleurs;
 
+import iut.gon.gribouille_tp1.modele.Trace;
 import javafx.scene.input.MouseEvent;
 
 public class OutilCrayon extends Outil{
@@ -8,16 +9,22 @@ public class OutilCrayon extends Outil{
 		super(controller);
 	}
 	
-	@Override
-	public void onMousePress(MouseEvent evt) {
-		// TODO Auto-generated method stub
+	public void onMousePress(double x, double y) {
+		
+			controller.precX.set(x); 
+	    	controller.precY.set(y);
+	    	this.trace = new Trace(1, "black", x, y);
+	    	controller.dessin.addFigure(trace);
 		
 	}
 
-	@Override
-	public void onMouseDrag(MouseEvent evt) {
-		// TODO Auto-generated method stub
+
+	public void onMouseDrag(double x, double y) {
+		canvas.getGraphicsContext2D().strokeLine(prevX.getValue(), prevY.getValue(), evt.getX(), evt.getY());
+    	trace.addPoint(evt.getX(), evt.getY());
+    	this.prevX.set(evt.getX());
+    	this.prevY.set(evt.getY());
 		
 	}
-	
+
 }
